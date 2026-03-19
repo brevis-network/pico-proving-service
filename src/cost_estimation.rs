473c63aae5b8777b2f98992f79e0fa3277438779
kaiwei-0 <kaiwei@brevis.network>
2026-03-19 10:19:26 +0800

    fix: temp fix

--- src/cost_estimation.rs
+++ src/cost_estimation.rs
@@ -1,5 +1,3 @@
-pub mod cache;
-
 use crate::{
     error::PicoError,
     types::{SC, Val},
@@ -79,6 +77,7 @@ pub fn estimate_cost(
         let (reports, total_cycles, pv_stream) =
             emulate_snapshot_pipeline(&witness, move |rec, _done| {
                 let mut counts = counts_ref.lock().unwrap();
+                // TODO: make events public
                 for (syscall_code, events) in rec.precompile_events.events.iter() {
                     if !events.is_empty() {
                         *counts.entry(format!("{:?}", syscall_code)).or_insert(0) +=
